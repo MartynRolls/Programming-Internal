e2b913dae2891307baef9b68452566f81edd1a7b
MartynRolls <162859424+MartynRolls@users.noreply.github.com>
2024-05-05 20:47:24 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,29 @@
 import pygame
 import game
 
+# Initialising game
+pygame.init()
+
+# Defining Variables
+width, height = 750, 500
+screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Window Title')
 
 level = game.Level()
 
-# Main loopwhile True:
-    # Checking events    for event in pygame.event.get():
+# Main loop
+while True:
+    # Checking events
+    for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
     screen.fill('white')
 
-    # Checking key presses    keys = pygame.key.get_pressed()
+    # Checking key presses
+    keys = pygame.key.get_pressed()
 
     image = level.draw_level(25)
     screen.blit(image, (0, 0))
