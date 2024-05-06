fa41f769331573dee6b45ef5b8d1b80405ac931f
MartynRolls <162859424+MartynRolls@users.noreply.github.com>
2024-05-06 18:07:16 +1200

    Update game.py

--- game.py
+++ game.py
@@ -3,15 +3,59 @@ import pygame
 
 class Player:
     def __init__(self):
-        self.level = []
-        self.x = 0
-        self.y = 0
+        self.collision_box = pygame.Rect(0, 0, 25, 25)
+        self.level_map = []
+        self.x = 350
+        self.y = 250
+        self.dx = 0
+        self.dy = 0
+        self.airborne = False
 
     def set_level(self, level):
-        self.level = level
+        self.level_map = level
 
-    def move_x(self, dx):
-        pass
+    def move(self):
+        self.x += self.dx
+
+        while self.collision():
+            self.x -= self.dx
+
+        if self.airborne:
+            self.fall()
+
+        self.collision_box.bottomleft = self.x, self.y
+
+    def jump(self):
+        self.airborne = True
+        self.dy = -10
+
+    def fall(self):
+        self.y += self.dy
+        self.dy += 1
+        if self.collision():
+            self.y = int(self.y)
+
+            if self.dy < 0:
+                while self.collision():
+                    self.y += 1
+            else:
+                while self.collision():
+                    self.y -= 1
+                self.airborne = False
+
+            self.dy = 0
+
+    def collision(self):
+        self.collision_box.bottomleft = self.x, self.y
+        for y in range(int(self.y / 25 - 1), int(self.y / 25 + 1)):
+            for x in range(int(self.x / 25 - 1), int(self.x / 25 + 1)):
+                collide = self.collision_box.colliderect((x * 25, y * 25, 25, 25))
+                try:
+                    if collide and self.level_map[y][x] == 1:
+                        return True
+                except IndexError:
+                    pass
+        return False
 
 
 class Level:
@@ -30,12 +74,12 @@ class Level:
     0 = Empty Space
     1 = Wall
     '''
-    def draw_level(self, tile_size):
-        image = pygame.Surface((tile_size*30, tile_size*20))
-        pygame.draw.rect(image, 'white', (0, 0, 30 * tile_size, 20 * tile_size))
+    def draw_level(self):
+        image = pygame.Surface((750, 500))
+        pygame.draw.rect(image, 'white', (0, 0, 750, 500))
         for y, line in enumerate(self.level_map):
             for x, tile in enumerate(line):
                 if tile == 1:
-                    pygame.draw.rect(image, 'black', (x*tile_size, y*tile_size, tile_size, tile_size))
+                    pygame.draw.rect(image, 'black', (x*25, y*25, 25, 25))
 
         return image
