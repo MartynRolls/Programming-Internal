7368abed9e888752cb73ef014e9fd06726253a2f
MartynRolls <162859424+MartynRolls@users.noreply.github.com>
2024-05-06 18:07:48 +1200

    Update main.py

--- main.py
+++ main.py
@@ -11,6 +11,9 @@ clock = pygame.time.Clock()
 pygame.display.set_caption('Window Title')
 
 level = game.Level()
+player = game.Player()
+
+player.set_level(level.level_map)
 
 # Main loop
 while True:
@@ -23,10 +26,20 @@ while True:
     screen.fill('white')
 
     # Checking key presses
+    player.dx = 0
     keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player.jump()
+    if keys[pygame.K_a]:
+        player.dx -= 10
+    if keys[pygame.K_d]:
+        player.dx += 10
 
-    image = level.draw_level(25)
+    player.move()
+
+    image = level.draw_level()
     screen.blit(image, (0, 0))
+    pygame.draw.rect(screen, 'red', player.collision_box)
 
     pygame.display.flip()
     clock.tick(60)
