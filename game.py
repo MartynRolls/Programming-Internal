9568c8593015a3281308a39e311c74f42e1ef3e9
MartynRolls <162859424+MartynRolls@users.noreply.github.com>
2024-05-08 18:04:12 +1200

    Update game.py

--- game.py
+++ game.py
@@ -17,53 +17,57 @@ class Player:
     def move(self):
         self.x += self.dx
 
-        while self.collision():
-            self.x += 1 if self.dx < 0 else -1
+        '''while self.collision_wall():
+            self.x += 1 if self.dx < 0 else -1'''
 
         if self.airborne:
             self.fall()
 
-        self.y += 1
-        if self.collision():
-            self.y -= 1
-        else:
+        if not self.collision(0, 1):
             self.airborne = True
 
         self.collision_box.topleft = self.x, self.y
 
     def jump(self):
-        self.y += 5
-        if self.collision():
+        if self.collision(0, 5) and not self.airborne:
             self.airborne = True
             self.dy = -12
-        self.y -= 5
 
     def fall(self):
-        self.y += self.dy
-        self.dy += 1
-        if self.collision():
-            self.y = int(self.y)
-
+        if self.collision(0, self.dy):
             if self.dy < 0:
-                while self.collision():
-                    self.y += 1
+                while self.collision(0, self.dy):
+                    self.dy += 1
             else:
-                while self.collision():
-                    self.y -= 1
+                while self.collision(0, self.dy):
+                    self.dy -= 1
                 self.airborne = False
 
+            self.y += self.dy
             self.dy = 0
+        else:
+            self.y += self.dy
+            self.dy += 1
 
-    def collision(self):
-        self.collision_box.topleft = self.x, self.y
+    def collision(self, dx, dy):
+        self.collision_box.topleft = self.x + dx, self.y + dy
         for y in range(int(self.y / 25), int(self.y / 25 + 3)):
             for x in range(int(self.x / 25), int(self.x / 25 + 3)):
                 collide = self.collision_box.colliderect(pygame.Rect(x * 25, y * 25, 25, 25))
                 if collide:
-                    if ((self.level_map[y][x] == 1) or
-                            (self.level_map[y][x] == 2 and self.dy >= 0)):
-
+                    if self.level_map[y][x] == 1:
                         return True
+                    if self.level_map[y][x] == 2 and self.y - dy + 25 < y*25:
+                        return True
+        return False
+
+    def collision_wall(self):
+        self.collision_box.topleft = self.x, self.y
+        for y in range(int(self.y / 25), int(self.y / 25 + 3)):
+            for x in range(int(self.x / 25), int(self.x / 25 + 3)):
+                collide = self.collision_box.colliderect(pygame.Rect(x * 25, y * 25, 25, 25))
+                if collide and self.level_map[y][x] == 1:
+                    return True
         return False
 
 
