dfab80e4f7710b2d1b5e3cd7f34bd1e2da5c1f4c
MartynRolls <162859424+MartynRolls@users.noreply.github.com>
2024-05-08 19:28:56 +1200

    Update game.py

--- game.py
+++ game.py
@@ -5,11 +5,11 @@ class Player:
     def __init__(self):
         self.collision_box = pygame.Rect(0, 0, 25, 25)
         self.level_map = []
+        self.airborne = False
         self.x = 350
         self.y = 250
         self.dx = 0
         self.dy = 0
-        self.airborne = False
 
     def set_level(self, level):
         self.level_map = level
@@ -17,13 +17,12 @@ class Player:
     def move(self):
         self.x += self.dx
 
-        '''while self.collision_wall():
-            self.x += 1 if self.dx < 0 else -1'''
+        while self.collision(0, 0):
+            self.x += 1 if self.dx < 0 else -1
 
         if self.airborne:
             self.fall()
-
-        if not self.collision(0, 1):
+        elif not self.collision(0, 1):
             self.airborne = True
 
         self.collision_box.topleft = self.x, self.y
@@ -51,23 +50,14 @@ class Player:
 
     def collision(self, dx, dy):
         self.collision_box.topleft = self.x + dx, self.y + dy
-        for y in range(int(self.y / 25), int(self.y / 25 + 3)):
-            for x in range(int(self.x / 25), int(self.x / 25 + 3)):
+        for y in range(int(self.y / 25 - 1), int(self.y / 25 + 3)):
+            for x in range(int(self.x / 25), int(self.x / 25 + 2)):
                 collide = self.collision_box.colliderect(pygame.Rect(x * 25, y * 25, 25, 25))
                 if collide:
                     if self.level_map[y][x] == 1:
                         return True
                     if self.level_map[y][x] == 2 and self.y - dy + 25 < y*25:
                         return True
-        return False
-
-    def collision_wall(self):
-        self.collision_box.topleft = self.x, self.y
-        for y in range(int(self.y / 25), int(self.y / 25 + 3)):
-            for x in range(int(self.x / 25), int(self.x / 25 + 3)):
-                collide = self.collision_box.colliderect(pygame.Rect(x * 25, y * 25, 25, 25))
-                if collide and self.level_map[y][x] == 1:
-                    return True
         return False
 
 
