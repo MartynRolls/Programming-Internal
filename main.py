ca6f751bc04f640ba415bea5854b769467fd9b33
MartynRolls <162859424+MartynRolls@users.noreply.github.com>
2024-05-14 07:34:13 +1200

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,8 @@ import game
 pygame.init()
 
 # Defining Variables
-width, height = 750, 500
+size = 2
+width, height = size * 300, size * 200
 screen = pygame.display.set_mode((width, height))
 clock = pygame.time.Clock()
 pygame.display.set_caption('Window Title')
@@ -14,6 +15,8 @@ level = game.Level()
 player = game.Player()
 
 player.set_level(level.level_map)
+image = level.draw_level()
+image = pygame.transform.scale_by(image, (size, size))
 
 # Main loop
 while True:
@@ -23,23 +26,27 @@ while True:
             pygame.quit()
             exit()
 
-    screen.fill('white')
+    screen.fill((30, 60, 90))
 
     # Checking key presses
     player.dx = 0
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player.jump()
+    if keys[pygame.K_s]:
+        player.y += 1
+        if player.collision(0, 0):
+            player.y -= 1
     if keys[pygame.K_a]:
-        player.dx -= 10
+        player.dx -= 8
     if keys[pygame.K_d]:
-        player.dx += 10
+        player.dx += 8
 
     player.move()
 
-    image = level.draw_level()
     screen.blit(image, (0, 0))
     pygame.draw.rect(screen, 'red', player.collision_box)
 
     pygame.display.flip()
     clock.tick(60)
+    # print(clock.get_fps())
